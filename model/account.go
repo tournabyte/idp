26984a27e7fdd9f1e5e53b3554ba560680da9335
CountTails <74679703+CountTails@users.noreply.github.com>
2025-11-05 12:29:18 -0800

    [model]: add implementation of document creation and lookup for the accounts collection

--- model/account.go
+++ model/account.go
@@ -5,7 +5,11 @@
 package model
 
 import (
+	"context"
 	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 type Account struct {
@@ -13,4 +17,33 @@ type Account struct {
 	Email     string    `bson:"email"`
 	CreatedAt time.Time `bson:"created_at"`
 	UpdatedAt time.Time `bson:"updated_at"`
+}
+
+type TournabyteAccountRepository struct {
+	collection *mongo.Collection
+}
+
+func NewTournabyteAccountRepository(db *mongo.Database) *TournabyteAccountRepository {
+	return &TournabyteAccountRepository{
+		db.Collection("accounts"),
+	}
+}
+
+func (r *TournabyteAccountRepository) Create(ctx context.Context, account *Account) error {
+	now := time.Now()
+	account.CreatedAt = now
+	account.UpdatedAt = now
+
+	_, err := r.collection.InsertOne(ctx, account)
+	return err
+}
+
+func (r *TournabyteAccountRepository) FindOne(ctx context.Context, filter bson.M) (*Account, error) {
+	var account Account
+
+	err := r.collection.FindOne(ctx, filter).Decode(&account)
+	if err == mongo.ErrNoDocuments {
+		return nil, nil
+	}
+	return &account, err
 }
