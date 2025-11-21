1c710d1e3ab63e348df0215ab2261aed7b1be8ff
CountTails <74679703+CountTails@users.noreply.github.com>
2025-11-21 14:45:08 -0800

    [test: model] add tests verifying the decoding and encoding behavior of ErrorResponse

--- model/payload_test.go
+++ model/payload_test.go
@@ -17,12 +17,12 @@ type CreateAccountRequestDecodeTestSuite struct {
 	stream []byte
 }
 
-func TestValidCreateAccountRequestDecoding(t *testing.T) {
+func TestCreateAccountRequestDecoding(t *testing.T) {
 	suite.Run(t, new(CreateAccountRequestDecodeTestSuite))
 }
 
 func (s *CreateAccountRequestDecodeTestSuite) SetupTest() {
-	s.stream = []byte("{\"email\": \"test@example.com\"}")
+	s.stream = []byte("{\"email\":\"test@example.com\"}")
 }
 
 func (s *CreateAccountRequestDecodeTestSuite) TestDecodeToStructuredData() {
@@ -58,4 +58,54 @@ func (s *CreateAccountRequestEncodeTestSuite) TestEncodeStructuredDataToStream()
 
 	assert.Nil(s.T(), streamErr)
 	assert.Equal(s.T(), []byte("{\"email\":\"test@example.com\"}"), stream)
+}
+
+type ErrorResponseDecodeTestSuite struct {
+	suite.Suite
+	stream []byte
+}
+
+func TestErrorResponseDecoding(t *testing.T) {
+	suite.Run(t, new(ErrorResponseDecodeTestSuite))
+}
+
+func (s *ErrorResponseDecodeTestSuite) SetupTest() {
+	s.stream = []byte("{\"reason\":\"OOPS\",\"err_msg\":\"because of this\"}")
+}
+
+func (s *ErrorResponseDecodeTestSuite) TestDecodeToStructuredData() {
+	var result ErrorResponse
+	if assert.Nil(s.T(), json.Unmarshal(s.stream, &result)) {
+		assert.Equal(s.T(), "OOPS", result.Reason)
+		assert.Equal(s.T(), "because of this", result.Message)
+	}
+}
+
+func (s *ErrorResponseDecodeTestSuite) TestDecodeFailsWithMisnamedField() {
+	s.stream[21] = ' ' // changes "err_msg" key to "err msg"
+	var result ErrorResponse
+
+	assert.Nil(s.T(), json.Unmarshal(s.stream, &result))
+	assert.Equal(s.T(), result.Reason, "OOPS") // Field still populated
+	assert.Equal(s.T(), result.Message, "")    // Field was not populated
+}
+
+type ErrorResponseEncodeTestSuite struct {
+	suite.Suite
+	value ErrorResponse
+}
+
+func TestErrorResponseEncoding(t *testing.T) {
+	suite.Run(t, new(ErrorResponseEncodeTestSuite))
+}
+
+func (s *ErrorResponseEncodeTestSuite) SetupTest() {
+	s.value = ErrorResponse{Reason: "OOPS", Message: "because of this"}
+}
+
+func (s *ErrorResponseEncodeTestSuite) TestEncodeStructuredDataToStream() {
+	stream, streamErr := json.Marshal(s.value)
+
+	assert.Nil(s.T(), streamErr)
+	assert.Equal(s.T(), []byte("{\"reason\":\"OOPS\",\"err_msg\":\"because of this\"}"), stream)
 }
